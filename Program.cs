0e0e4644e534ea487442c7e696a9e0e16ef4b3e8
GhostX <egamovkhurshed29@gmail.com>
2020-04-21 21:25:27 +0500

    project inited

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using CABS.Models;
+
+namespace CABS
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
