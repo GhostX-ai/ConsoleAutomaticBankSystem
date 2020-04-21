21c4a9b50ff3bb4c7c4fde2e236c3adea1e02694
GhostX <egamovkhurshed29@gmail.com>
2020-04-22 03:53:19 +0500

    Added Welcomung method with some misstakes in future they will not be there

--- Program.cs
+++ Program.cs
@@ -5,9 +5,38 @@ namespace CABS
 {
     class Program
     {
+        UAccaount ua = new UAccaount();
         static void Main()
         {
-            
+
+        }
+        static void Welcoming()
+        {
+            Console.Write("1-для регистрации\n2-для входа\n3-для выхода");
+            int chs = 0;
+            try
+            {
+                chs = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Welcoming();
+            }
+            Console.WriteLine();
+            switch (chs)
+            {
+                case 1:
+                    {
+                        SingUp();
+                    }
+                    break;
+                case 2:
+                    {
+                        SingIn();
+                    }
+                    break;
+            }
         }
     }
 }
